36376a5c0953f8491f7de9071159afe6aa565323
j3003att-beep <j3003att@sit.ac.jp>
2025-11-26 14:42:18 +0900

    テスト,tesy.bash追加

--- test.bash
+++ test.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrighitText: 2022 Ryuichi Ueda
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng(){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/0 TEST ###
 
 out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng ${LINENO}
 
-[ "${out}" = 15 ]
+[ "$res" = 0 ] && echo OK
+exit $res
