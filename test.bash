5856d77fa52a2c38498c9cf680946ae16cf11e75
j3003att-beep <j3003att@sit.ac.jp>
2025-12-03 14:21:39 +0900

    fix empty line handling

--- test.bash
+++ test.bash
@@ -3,25 +3,24 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng(){
-	echo NG at Line $1
-	res=1
+    echo NG at Line $1
+    res=1
 }
 
 res=0
 
-### I/0 TEST ###
+### I/O TEST ###
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ] || ng ${LINENO}
+[ "${out}" = "15.0" ] || ng ${LINENO}
 
 ### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ] ||ng ${LINENO}
-[ "${out}"=""}||ng ${LINENO}
 
-out=$(echo | ./plus)
-[ "$? = 1 ]	||ng ${LINENO}
-[ "${out}" = ""]||ng ${LINENO}
+# 「あ」など数字以外が入ったらエラーが起きるのが正しい
+echo "あ" | ./plus >/dev/null 2>&1
+[ "$?" = "1" ] || ng ${LINENO}
 
-[ "$res" = 0 ] && echo OK
-exit $res
+# 出力は空であるべき
+out=$(echo "あ" | ./plus 2>/dev/null)
+[ "${out}" = "" ] || ng ${
+
